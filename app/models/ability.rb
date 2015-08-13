b60fb7fb234bf9eac3b48a8bcea8f89e8b1f57b7
Eric Conde <condeeric@live.com>
2015-08-13 16:50:41 +0800

    Add admin ability. Add default seed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
 
     if user.admin?
-
+      can :manage, :all
     else
       can :update, Question do |question|
         question.user == user
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
-User.create(email: 'eric@yahoo.com', password: 'eric1234')
-User.create(email: 'eric2@yahoo.com', password: 'eric1234')
+u1 = User.create(email: 'u1@yahoo.com', password: 'user1234', admin: false)
+u2 = User.create(email: 'u2@yahoo.com', password: 'user1234', admin: false)
+User.create(email: 'admin@yahoo.com', password: 'admin1234', admin: true)
+
+q1 = u1.questions.create(content: "What's your favorite programming language?")
+
+Answer.create(user_id: u1.id, question_id: q1.id, content: "I love Ruby on Rails!").save
+Answer.create(user_id: u2.id, question_id: q1.id, content: "Javascript Rocks!").save
