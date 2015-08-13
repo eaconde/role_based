3df83ab936bbf07df3ab74dcc1477a3dfb8f8bfc
Eric Conde <condeeric@live.com>
2015-08-13 16:52:27 +0800

    Add admin ability. Add default seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
+# seed users
 u1 = User.create(email: 'u1@yahoo.com', password: 'user1234', admin: false)
 u2 = User.create(email: 'u2@yahoo.com', password: 'user1234', admin: false)
 User.create(email: 'admin@yahoo.com', password: 'admin1234', admin: true)
-
+# seed question
 q1 = u1.questions.create(content: "What's your favorite programming language?")
-
-Answer.create(user_id: u1.id, question_id: q1.id, content: "I love Ruby on Rails!").save
-Answer.create(user_id: u2.id, question_id: q1.id, content: "Javascript Rocks!").save
+# seed answers
+q1.answers.create(user_id: u1.id, content: "I love Ruby on Rails!")
+q1.answers.create(user_id: u2.id, content: "Javascript Rocks!")
